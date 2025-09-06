b38ba3a8314493a07e5ebb494a32e2a70bb9596c
a73bf6328091377c572eb0c2248dc13f60c5a70f <imran.myid@gmail.com>
2025-09-07 00:23:26 +0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,20 @@
-// tailwind.config.ts
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        bg: "#0b0b12",
+        text: "#e5e7eb"
+      }
+    }
+  },
   plugins: []
-}
-export default config
+};
+
+export default config;
